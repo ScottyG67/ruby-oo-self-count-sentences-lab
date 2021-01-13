f43ef9314b74eff825359a2aa97ea3f0bc3db7d6
github username <scott.gloyna01@gmail.com>
2021-01-12 20:40:47 -0600

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,18 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    self.split.select{|s| s.sentence? or s.question? or s.exclamation?}.length
   end
 end
